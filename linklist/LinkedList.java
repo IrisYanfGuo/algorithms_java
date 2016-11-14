ee6fdf7fc6a8f46359b329ce361e0d98df68a64e
Iris <yanfguo@vub.ac.be>
2016-11-14 15:04:36 +0100

    linkedlist

--- linklist/LinkedList.java
+++ linklist/LinkedList.java
@@ -1,35 +1,68 @@
-package linklist;
+import java.util.Comparator;
 
-
-
+/**
+ * The type Linked list.
+ */
 public class LinkedList {
 
-	private class ListElement {
+	private  class ListElement {
 		private Object el1;
 		private ListElement el2;
 
+		/**
+		 * Instantiates a new List element.
+		 * Use a inner class,
+		 *
+		 * @param el          the el, the object
+		 * @param nextElement the next element
+		 */
 		public ListElement(Object el, ListElement nextElement) {
 			el1 = el;
 			el2 = nextElement;
 		}
 
+		/**
+		 * Instantiates a new List element with only one parameter, with the nextelement is null.
+		 *
+		 * @param el the el
+		 */
 		public ListElement(Object el) {
 			this(el, null);
 		}
 
+		/**
+		 * First object.
+		 *
+		 * @return the object, the contant in each unit.
+		 */
 		public Object first() {
 			return el1;
 		}
 
+		/**
+		 * Rest list element.
+		 *
+		 * @return the the nextelement ,which points to the rest of the list
+		 */
 		public ListElement rest() {
 			return el2;
 		}
 
+		/**
+		 * Sets the el1
+		 *
+		 * @param value the value
+		 */
 		@SuppressWarnings("unused")
 		public void setFirst(Object value) {
 			el1 = value;
 		}
 
+		/**
+		 * Sets the el2,which  indicates the rest elements
+		 *
+		 * @param value the value
+		 */
 		public void setRest(ListElement value) {
 			el2 = value;
 		}
@@ -39,21 +72,42 @@ public class LinkedList {
 		 */
 	}
 
+
 	private ListElement head;
 
+	/**
+	 * Instantiates a new Linked list.
+	 */
 	public LinkedList() {
 		head = null;
 	}
 
+
+	/**
+	 * Add first, add from the head.
+	 *
+	 * @param o the o
+	 */
 	public void addFirst(Object o) {
 		head = new ListElement(o, head);
 
 	}
 
+	/**
+	 * Gets first.
+	 *
+	 * @return the first
+	 */
 	public Object getFirst() {
 		return head.first();
 	}
 
+	/**
+	 * Get object with index n.
+	 *
+	 * @param n the n
+	 * @return the object
+	 */
 	public Object get(int n) {
 		ListElement d = head;
 		while (n > 0) {
@@ -75,7 +129,12 @@ public class LinkedList {
 		return s;
 	}
 
-	/*
+	/**
+	 * Print linked list.
+	 *
+	 * @return the linked list
+	 */
+/*
 	 * public void print() { LinkedList.ListElement temp; temp = head; do {
 	 * System.out.println(temp.el1); temp=temp.el2; }while (temp!=null); }
 	 */
@@ -85,6 +144,11 @@ public class LinkedList {
 		return this;
 	}
 
+	/**
+	 * return the size of the linked list
+	 *
+	 * @return the int
+	 */
 	public int size() {
 		int size = 0;
 		ListElement t = head;
@@ -95,7 +159,14 @@ public class LinkedList {
 		return size;
 	}
 
-	// Add a method set(int n, Object o) which replaces the nth element in the
+	/**
+	 * Set linkedlist element n with the index n
+	 *
+	 * @param index the index
+	 * @param o     the o
+	 * @return the linked list
+	 */
+// Add a method set(int n, Object o) which replaces the nth element in the
 	// list.
 	public LinkedList set(int index, Object o) {
 		int t = 1;
@@ -108,7 +179,12 @@ public class LinkedList {
 		return this;
 	}
 
-	// Add a method getLast() which returns the last element in the list.
+	/**
+	 * Get the el2 which point to the last element.
+	 *
+	 * @return the last
+	 */
+// Add a method getLast() which returns the last element in the list.
 	public ListElement getLast() {
 		ListElement p = head;
 		while (p.el2 != null) {
@@ -119,13 +195,25 @@ public class LinkedList {
 	// Add a method addLast() which adds an element to the end of the linked
 	// list.
 
+	/**
+	 * Add objects to linked list from the end
+	 *
+	 * @param o the o
+	 * @return the linked list
+	 */
 	public LinkedList addLast(Object o) {
 		ListElement t = getLast();
 		t.el2 = new ListElement(o);
 		return this;
 	}
 
-	// searches for a given element in the linked list.
+	/**
+	 * Search list element with the object given and return the el2 point to this.
+	 *
+	 * @param o the o
+	 * @return the list element
+	 */
+// searches for a given element in the linked list.
 	public ListElement search(Object o) {
 		ListElement p = head;
 		while (p.el1 != o) {
@@ -134,6 +222,31 @@ public class LinkedList {
 		return p;
 	}
 
+	/**
+	 * Pop first object.
+	 *
+	 * @return the object
+	 */
+	public Object popFirst(){
+		Object t = head.el1;
+		head= head.el2;
+		return t;
+	}
+
+	/**
+	 * Pop last object.
+	 *
+	 * @return the object
+	 */
+	public  Object popLast(){
+		ListElement p = head;
+		while (p.el2.el2 != null) {
+			p = p.el2;
+		}
+		Object t = p.rest().first();
+		p.setRest(null);
+		return t;
+	}
 	/*
 	 * public LinkedList fropple() { ListElement p = head; while(p!=null){
 	 * p.el2.el2 = p; p.el2 =p.el2.el2; p = p.el2.el2; } return this;
@@ -142,10 +255,20 @@ public class LinkedList {
 	 */ // error, infinite loop
 
 
+	/**
+	 * Append. add something to linkedlist. combine 2 linked list.
+	 *
+	 * @param l2 the l 2
+	 */
 	public void append(LinkedList l2) {
 		getLast().el2=l2.head;
 	}
-	
+
+	/**
+	 * Fropple linked list.
+	 *
+	 * @return the linked list
+	 */
 	public LinkedList fropple() {
 		if (head!=null&&head.rest()!=null) {
 			ListElement t1=head;
@@ -172,7 +295,12 @@ public class LinkedList {
 		}
 		return this;				
 	}
-	
+
+	/**
+	 * the second way to fropple linked list.
+	 *
+	 * @return the linked list
+	 */
 	public LinkedList fropple1() {
 		ListElement p =head;
 		while(p.rest()!=null&&p!=null){
@@ -183,6 +311,28 @@ public class LinkedList {
 			p=p.rest().rest();
 		}
 		return this;
+	}
+
+
+	/**
+	 * Add sorted. this is used in the Priority Queue. add with priority.
+	 *
+	 * @param o the o
+	 */
+	public void addSorted(Comparable o){
+		if(head == null){
+			head = new ListElement(o,null);
+		} else  if (((Comparable)head.first()).compareTo(o)>0){
+			head = new ListElement(o,head);
+		} else{
+			ListElement d = head;
+			while((d.rest()!= null)&&((Comparable)d.rest().first()).compareTo(o)<0){
+				d=d.rest();
+			}
+			ListElement next = d.rest();
+			d.setRest(new ListElement(o,next));
+		}
+
 	}
 
 }
--- socialNet/LinkedList.java
+++ socialNet/LinkedList.java
@@ -1,15 +1,7 @@
-package socialNetworks;
+import java.util.Comparator;
 
-/**a linked list is a linear collection of data elements, called nodes, each pointing to the next 
- * node by means of a pointer. It is a data structure consisting of a group of nodes 
- * which together represent a sequence. Under the simplest form, each node is composed of 
- * data and a reference (in other words, a link) to the next node in the sequence. This structure allows 
- * for efficient insertion or removal of elements from any position in the sequence during iteration. 
- * More complex variants add additional links, allowing efficient insertion or removal from arbitrary element references.
- * to know more details visit: https://en.wikipedia.org/wiki/Linked_list
- * 
- * version:1.0 time 7th Nov
- * @author Yanfang Guo <yanfguo@outlook.com> <yanfguo@vub.ac.be>
+/**
+ * The type Linked list.
  */
 public class LinkedList {
 
@@ -17,28 +9,60 @@ public class LinkedList {
 		private Object el1;
 		private ListElement el2;
 
+		/**
+		 * Instantiates a new List element.
+		 * Use a inner class,
+		 *
+		 * @param el          the el, the object
+		 * @param nextElement the next element
+		 */
 		public ListElement(Object el, ListElement nextElement) {
 			el1 = el;
 			el2 = nextElement;
 		}
 
+		/**
+		 * Instantiates a new List element with only one parameter, with the nextelement is null.
+		 *
+		 * @param el the el
+		 */
 		public ListElement(Object el) {
 			this(el, null);
 		}
 
+		/**
+		 * First object.
+		 *
+		 * @return the object, the contant in each unit.
+		 */
 		public Object first() {
 			return el1;
 		}
 
+		/**
+		 * Rest list element.
+		 *
+		 * @return the the nextelement ,which points to the rest of the list
+		 */
 		public ListElement rest() {
 			return el2;
 		}
 
+		/**
+		 * Sets the el1
+		 *
+		 * @param value the value
+		 */
 		@SuppressWarnings("unused")
 		public void setFirst(Object value) {
 			el1 = value;
 		}
 
+		/**
+		 * Sets the el2,which  indicates the rest elements
+		 *
+		 * @param value the value
+		 */
 		public void setRest(ListElement value) {
 			el2 = value;
 		}
@@ -48,21 +72,42 @@ public class LinkedList {
 		 */
 	}
 
+
 	private ListElement head;
 
+	/**
+	 * Instantiates a new Linked list.
+	 */
 	public LinkedList() {
 		head = null;
 	}
 
+
+	/**
+	 * Add first, add from the head.
+	 *
+	 * @param o the o
+	 */
 	public void addFirst(Object o) {
 		head = new ListElement(o, head);
 
 	}
 
+	/**
+	 * Gets first.
+	 *
+	 * @return the first
+	 */
 	public Object getFirst() {
 		return head.first();
 	}
 
+	/**
+	 * Get object with index n.
+	 *
+	 * @param n the n
+	 * @return the object
+	 */
 	public Object get(int n) {
 		ListElement d = head;
 		while (n > 0) {
@@ -73,17 +118,23 @@ public class LinkedList {
 	}
 
 	public String toString() {
-		String s = "";
+		String s = "(";
 		ListElement d = head;
 		while (d != null) {
 			s += d.first().toString();
 			s += " ";
 			d = d.rest();
 		}
+		s += ")";
 		return s;
 	}
 
-	/*
+	/**
+	 * Print linked list.
+	 *
+	 * @return the linked list
+	 */
+/*
 	 * public void print() { LinkedList.ListElement temp; temp = head; do {
 	 * System.out.println(temp.el1); temp=temp.el2; }while (temp!=null); }
 	 */
@@ -93,6 +144,11 @@ public class LinkedList {
 		return this;
 	}
 
+	/**
+	 * return the size of the linked list
+	 *
+	 * @return the int
+	 */
 	public int size() {
 		int size = 0;
 		ListElement t = head;
@@ -103,7 +159,14 @@ public class LinkedList {
 		return size;
 	}
 
-	// Add a method set(int n, Object o) which replaces the nth element in the
+	/**
+	 * Set linkedlist element n with the index n
+	 *
+	 * @param index the index
+	 * @param o     the o
+	 * @return the linked list
+	 */
+// Add a method set(int n, Object o) which replaces the nth element in the
 	// list.
 	public LinkedList set(int index, Object o) {
 		int t = 1;
@@ -116,7 +179,12 @@ public class LinkedList {
 		return this;
 	}
 
-	// Add a method getLast() which returns the last element in the list.
+	/**
+	 * Get the el2 which point to the last element.
+	 *
+	 * @return the last
+	 */
+// Add a method getLast() which returns the last element in the list.
 	public ListElement getLast() {
 		ListElement p = head;
 		while (p.el2 != null) {
@@ -127,13 +195,25 @@ public class LinkedList {
 	// Add a method addLast() which adds an element to the end of the linked
 	// list.
 
+	/**
+	 * Add objects to linked list from the end
+	 *
+	 * @param o the o
+	 * @return the linked list
+	 */
 	public LinkedList addLast(Object o) {
 		ListElement t = getLast();
 		t.el2 = new ListElement(o);
 		return this;
 	}
 
-	// searches for a given element in the linked list.
+	/**
+	 * Search list element with the object given and return the el2 point to this.
+	 *
+	 * @param o the o
+	 * @return the list element
+	 */
+// searches for a given element in the linked list.
 	public ListElement search(Object o) {
 		ListElement p = head;
 		while (p.el1 != o) {
@@ -142,6 +222,31 @@ public class LinkedList {
 		return p;
 	}
 
+	/**
+	 * Pop first object.
+	 *
+	 * @return the object
+	 */
+	public Object popFirst(){
+		Object t = head.el1;
+		head= head.el2;
+		return t;
+	}
+
+	/**
+	 * Pop last object.
+	 *
+	 * @return the object
+	 */
+	public  Object popLast(){
+		ListElement p = head;
+		while (p.el2.el2 != null) {
+			p = p.el2;
+		}
+		Object t = p.rest().first();
+		p.setRest(null);
+		return t;
+	}
 	/*
 	 * public LinkedList fropple() { ListElement p = head; while(p!=null){
 	 * p.el2.el2 = p; p.el2 =p.el2.el2; p = p.el2.el2; } return this;
@@ -150,10 +255,20 @@ public class LinkedList {
 	 */ // error, infinite loop
 
 
+	/**
+	 * Append. add something to linkedlist. combine 2 linked list.
+	 *
+	 * @param l2 the l 2
+	 */
 	public void append(LinkedList l2) {
 		getLast().el2=l2.head;
 	}
-	
+
+	/**
+	 * Fropple linked list.
+	 *
+	 * @return the linked list
+	 */
 	public LinkedList fropple() {
 		if (head!=null&&head.rest()!=null) {
 			ListElement t1=head;
@@ -180,7 +295,12 @@ public class LinkedList {
 		}
 		return this;				
 	}
-	
+
+	/**
+	 * the second way to fropple linked list.
+	 *
+	 * @return the linked list
+	 */
 	public LinkedList fropple1() {
 		ListElement p =head;
 		while(p.rest()!=null&&p!=null){
@@ -191,6 +311,28 @@ public class LinkedList {
 			p=p.rest().rest();
 		}
 		return this;
+	}
+
+
+	/**
+	 * Add sorted. this is used in the Priority Queue. add with priority.
+	 *
+	 * @param o the o
+	 */
+	public void addSorted(Comparable o){
+		if(head == null){
+			head = new ListElement(o,null);
+		} else  if (((Comparable)head.first()).compareTo(o)>0){
+			head = new ListElement(o,head);
+		} else{
+			ListElement d = head;
+			while((d.rest()!= null)&&((Comparable)d.rest().first()).compareTo(o)<0){
+				d=d.rest();
+			}
+			ListElement next = d.rest();
+			d.setRest(new ListElement(o,next));
+		}
+
 	}
 
 }
